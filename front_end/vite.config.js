b6cccf1b628adf3993f22a99b8a81711db2d52fc
ahkhan13 <khanahs3351@gmail.com>
2024-12-17 22:11:59 +0530

    add cors origin

--- front_end/vite.config.js
+++ front_end/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Vite's default port
+    proxy: {
+      // Proxy all API requests during local development
+      '/api': {
+        target:'https://travelapp-jy34.onrender.com',
+      },
+    },
+  }
 })
